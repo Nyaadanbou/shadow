fac829bc7b3acba909dfc84380a6b8d21067278f
Akiranya <porridgen@gmail.com>
2024-01-24 23:15:33 +0800

    rewrite build scripts for mewcraft

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "cc.mewcraft"
+version = "1.4"
+
+java {
+    withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.checkerframework", "checker-qual", "3.42.0")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.9.1")
+    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.9.1")
+    testImplementation("org.checkerframework", "checker-qual", "3.42.0")
+}
+
+val userHome: String = when {
+    System.getProperty("os.name").startsWith("Windows", ignoreCase = true) -> System.getenv("USERPROFILE")
+    else -> System.getenv("HOME")
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("$userHome/MewcraftRepository")
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
